40776c5a4d9344ea19712522354902f59e7de054
Vishwas <im.vishwas.03@gmail.com>
2023-10-05 00:29:18 +0530

    initialized playgound

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>awkward-student 🎓</title>
+        <meta name="description" content="" />
+        <link rel="icon" href="../public/favicon.ico" />
+      </Head>
+      <main></main>
+    </div>
+  );
+}
