425fde46f3f8067754bec0c0f7efc8d4bd065ea1
Vishwas <im.vishwas.03@gmail.com>
2024-04-30 02:34:58 +0530

    Create Indx.jsx

---
+++ src/components/Indx.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "../css/namaste.css";
+
+function Indx() {
+  return (
+    <div class="namaste-text-div" id="ui">
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+    </div>
+  );
+}
+
+export default Indx;
