ab30d67e74519c21830763b59cae4129dc688c56
Vishwas <im.vishwas.03@gmail.com>
2024-04-30 02:35:13 +0530

    Create Skills.jsx

---
+++ src/components/Skills.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Skills() {
+  return (
+    <section id="skills">
+      <h2>What's under the hood?</h2>
+      <h3>The tools I use behind the scenes.</h3>
+      <ul id="skills-list">
+        <li>JAVA</li>
+        <li>SPRING</li>
+        <li>SQL</li>
+        <li>JAVA SCRIPT</li>
+        <li>NODE</li>
+        <li>NPM</li>
+        <li>MONGO</li>
+        <li>PYTHON</li>
+        <li>REACT</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Skills;
