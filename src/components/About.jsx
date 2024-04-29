4bdd38c64f105ea3195c4b672d7f485ecc36f270
Vishwas <im.vishwas.03@gmail.com>
2024-04-30 02:35:08 +0530

    Create About.jsx

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function About() {
+  return (
+    <section id="about">
+      <h2>A lil'bit bout me.</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+        in dolor earum ipsa voluptatibus ut, quod, laborum accusantium veniam
+        amet eum. Deserunt iure consequuntur asperiores iste voluptate vitae,
+        aut deleniti?
+      </p>
+    </section>
+  );
+}
+
+export default About;
