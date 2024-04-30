da9f6c6b381782f81cfbc85a3363c10b222561da
Vishwas <im.vishwas.03@gmail.com>
2024-04-30 23:33:54 +0530

    Update Skills.jsx & Create skills.css
    Created skill cards and divs.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,21 +1,28 @@
 import React from "react";
+import "../css/skills.css";
 
 function Skills() {
   return (
     <section id="skills">
       <h2>What's under the hood?</h2>
-      <h3>The tools I use behind the scenes.</h3>
-      <ul id="skills-list">
-        <li>JAVA</li>
-        <li>SPRING</li>
-        <li>SQL</li>
-        <li>JAVA SCRIPT</li>
-        <li>NODE</li>
-        <li>NPM</li>
-        <li>MONGO</li>
-        <li>PYTHON</li>
-        <li>REACT</li>
-      </ul>
+      <p>The tools I use behind the scenes.</p>
+      <div id="skills-div">
+        <div className="skills-card-container">
+          <div className="skill-card">JAVA</div>
+          <div className="skill-card">SPRING</div>
+          <div className="skill-card">SQL</div>
+        </div>
+        <div className="skills-card-container">
+          <div className="skill-card">JAVA SCRIPT</div>
+          <div className="skill-card">NODE</div>
+          <div className="skill-card">NPM</div>
+        </div>
+        <div className="skills-card-container">
+          <div className="skill-card">PYTHON</div>
+          <div className="skill-card">REACT</div>
+          <div className="skill-card">MONGO</div>
+        </div>
+      </div>
     </section>
   );
 }
