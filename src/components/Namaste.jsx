0289950d4d1e88f95e72509898efdbce54cbad63
Vishwas <im.vishwas.03@gmail.com>
2024-04-30 21:59:45 +0530

    Create Namaste.jsx
    Initialized Namaste.jsx as a new component after removing it from Indx.jsx

---
+++ src/components/Namaste.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import "../css/namaste.css";
+
+function Namaste() {
+  return (
+    <div class="namaste-text-div" id="ui">
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+      <div class="text">नमस्ते</div>
+    </div>
+  );
+}
+
+export default Namaste;
