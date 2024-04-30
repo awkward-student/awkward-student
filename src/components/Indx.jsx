2de870d5dda3c36f7138016d346e7b3cb533f5d1
Vishwas <im.vishwas.03@gmail.com>
2024-04-30 22:01:42 +0530

    Update Indx.jsx
    Removed 'Namaste' div and imported and served as a different component.

--- src/components/Indx.jsx
+++ src/components/Indx.jsx
@@ -1,50 +1,27 @@
-import React from "react";
-import "../css/namaste.css";
+import React, { useState, useEffect } from "react";
+import Namaste from "./Namaste";
 
 function Indx() {
+  const [showComponent, setShowComponent] = useState(true); // Initial state
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowComponent(false);
+    }, 10500); // time interval
+
+    return () => clearTimeout(timeout); // Cleanup function to avoid memory leaks
+  }, []); // Emptying dependency array to ensures useEffect runs only once after mount
+
   return (
-    <div class="namaste-text-div" id="ui">
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-      <div class="text">नमस्ते</div>
-    </div>
+    <section id="indx">
+      {showComponent && <Namaste />}
+      {!showComponent && (
+        <div id="main-indx">
+          <h2>Hello World!</h2>
+          <p>This is Vishwas Karode.</p>
+        </div>
+      )}
+    </section>
   );
 }
 
